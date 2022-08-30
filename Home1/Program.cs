0312ba151083c5eab5ff9b7c387401ec8ad8b5ab
Dmitry-Lab <poznavshii@gmail.com>
2022-08-30 10:22:37 +0400

    Home1

---
+++ Home1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число А: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if(numB >= 0)
+{
+    double result = Math.Pow(numA, numB);           // numA возводим в степень numB 
+    Console.WriteLine(result);
+}
+else 
+{
+   Console.WriteLine("Число B не натуральное"); 
+}
