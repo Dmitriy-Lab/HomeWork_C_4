528990a528772291db0f6733e875365d14b41eed
Dmitry-Lab <poznavshii@gmail.com>
2022-08-30 10:18:57 +0400

    Home3

---
+++ Home2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма цифр = "+GetSum(num));     // Выводим результат
+
+int GetSum(int number)                              // Функция
+{
+    int sum = 0;
+    while (number > 0)
+        {
+        sum += number % 10;
+        number /= 10;
+         }
+    return sum;                                     // Возвращаем значение
+}
